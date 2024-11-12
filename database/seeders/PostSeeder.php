7c7996c94e33a1fb6a2bce8354eaa97150164243
orazi-paolo <orazi.paolo10@gmail.com>
2024-11-12 12:04:41 +0100

    update PostSeeder

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PostSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 20 ; $i++) {
+            $post = new Post();
+            $post->title = $faker->realTextBetween(10, 15);
+            $post->author = $faker->name();
+            $post->description = $faker->realTextBetween(100, 150);
+            $post->date = $faker->date();
+            $post->save();
+        }
     }
 }
