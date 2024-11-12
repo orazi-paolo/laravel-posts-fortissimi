ba07776681d493850ff450779fed6f20f06bab41
Riccardo Petricca <3359943+rc2pc2@users.noreply.github.com>
2024-11-12 12:40:29 +0100

    Update PostController.php
    Added secret dependency injection

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,9 +37,9 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        $post = Post::findOrFail($id);
+        // $post = Post::findOrFail($id); MUAHAHAHAHAAHAHA D.I. INTO ACTION
         return view("posts.show", compact("post"));
     }
 
